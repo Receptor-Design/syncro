595715af4ecaed7be2dd82a4eb13d80967d26abe
colinduwe <colinduwe@gmail.com>
2024-10-16 12:55:10 -0700

    added a couple started patterns for pages

---
+++ wp-content/themes/syncro-blocks/patterns/page-t2-7x.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Template 2 (7.x) (Resoures for...)
+ * Slug: syncro-blocks/page-t2-7x
+ * Categories: pages
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Hero T2"},"className":"hero-t2-pattern","layout":{"type":"constrained"}} -->
+<div class="wp-block-group hero-t2-pattern"><!-- wp:heading {"level":6} -->
+<h6 class="wp-block-heading" id="h-collection">Collection</h6>
+<!-- /wp:heading -->
+
+<!-- wp:post-title {"level":1} /-->
+
+<!-- wp:image {"id":28128,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://syncromsp.local/wp-content/uploads/2024/10/MSP-Topic-Hero.png" alt="" class="wp-image-28128"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|cream-4"}}}},"textColor":"cream-4"} -->
+<h3 class="wp-block-heading has-cream-4-color has-text-color has-link-color" id="h-as-an-msp-you-re-looking-to-grow-your-business-and-work-more-efficiently-you-re-looking-for-expert-insights-on-how-to-streamline-operations-while-delivering-excellent-customer-service-how-should-i-market-my-msp-how-can-i-boost-profitability-and-productivity-what-will-ai-mean-for-msps-today-and-in-the-near-future">As an MSP, you’re looking to grow your business and work more efficiently. You’re looking for expert insights on how to streamline operations while delivering excellent customer service. How should I market my MSP? How can I boost profitability and productivity? What will AI mean for MSPs today and in the near future?</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Collection Resourcs"},"align":"full","className":"collection-resources","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"cream-4","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull collection-resources has-base-color has-cream-4-background-color has-text-color has-background has-link-color"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading" id="h-here-s-a-collection-of-valuable-resources-authored-by-syncro-and-other-it-industry-experts-developed-to-answer-these-questions-and-more">Here’s a collection of valuable resources, authored by Syncro and other IT industry experts, developed to answer these questions and more.</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:syncro-blocks/selected-resources-grid {"name":"syncro-blocks/selected-resources-grid","data":{"selected_posts":["23396","27958","4042","21259","22469","22618","3982","21467"],"_selected_posts":"field_67047b9e2f79c"},"mode":"preview"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:block {"ref":28050} /-->
+
+<!-- wp:block {"ref":28052} /-->
