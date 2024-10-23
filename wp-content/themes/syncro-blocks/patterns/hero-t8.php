c1b0b43652cb9349fda5f3b021b2c0726e477441
colinduwe <colinduwe@gmail.com>
2024-10-23 11:02:50 -0700

    updated t8 pattern header

--- wp-content/themes/syncro-blocks/patterns/hero-t8.php
+++ wp-content/themes/syncro-blocks/patterns/hero-t8.php
@@ -2,15 +2,13 @@
 /**
  * Title: Hero T8
  * Slug: syncro-blocks/hero-t8
- * Categories: heros
- * Keywords: heros
- * Post Types: page
+ * Categories: Heros
  * Viewport width: 1400
  */
 ?>
 
-<!-- wp:cover {"url":"https://staging-syncromsp-staging.kinsta.cloud/wp-content/uploads/2024/10/Page-Header-T8_Platform-scaled.jpg","id":28566,"isUserOverlayColor":true,"minHeight":705,"gradient":"cream-4-page-hero-overlay","contentPosition":"bottom center","metadata":{"name":"Hero T8"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"bottom":"100px"}}},"textColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-center has-base-color has-text-color has-link-color" style="padding-bottom:100px;min-height:705px" id="hero-t8-pattern"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient has-cream-4-page-hero-overlay-gradient-background"></span><img class="wp-block-cover__image-background wp-image-28566" alt="" src="https://staging-syncromsp-staging.kinsta.cloud/wp-content/uploads/2024/10/Page-Header-T8_Platform-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<!-- wp:cover {"url":"<?php echo site_url(); ?>/wp-content/uploads/2024/10/Page-Header-T8_Platform-scaled.jpg","id":28566,"isUserOverlayColor":true,"minHeight":705,"gradient":"cream-4-page-hero-overlay","contentPosition":"bottom center","metadata":{"name":"Hero T8"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"bottom":"100px"}}},"textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-center has-base-color has-text-color has-link-color" style="padding-bottom:100px;min-height:705px" id="hero-t8-pattern"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient has-cream-4-page-hero-overlay-gradient-background"></span><img class="wp-block-cover__image-background wp-image-28566" alt="" src="<?php echo site_url(); ?>/wp-content/uploads/2024/10/Page-Header-T8_Platform-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"width":"951px"} -->
 <div class="wp-block-column" style="flex-basis:951px"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
 <h1 class="wp-block-heading" id="h-your-fully-integrated-msp-amp-it-management-platform" style="margin-top:var(--wp--preset--spacing--40)">Your Fully Integrated MSP &amp; IT Management Platform</h1>
