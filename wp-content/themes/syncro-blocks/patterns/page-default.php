5bc384d8becc1d5c16b520fafca84bda62cc7aa7
colinduwe <colinduwe@gmail.com>
2024-10-28 13:59:48 -0700

    added default page pattern

---
+++ wp-content/themes/syncro-blocks/patterns/page-default.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Default Page Pattern
+ * Slug: syncro-blocks/page-default
+ * Categories: pages, posts, resources
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"400px"} -->
+<div class="wp-block-column" style="flex-basis:400px"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:yoast-seo/table-of-contents {"maxHeadingLevel":2} -->
+<div class="wp-block-yoast-seo-table-of-contents yoast-table-of-contents"><h2>Table of contents</h2><ul><li><a href="#h-place-you-content-here" data-level="2">Place you content here</a></li></ul></div>
+<!-- /wp:yoast-seo/table-of-contents --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading -->
+<h2 class="wp-block-heading" id="h-place-you-content-here">Place you content here</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>whatever you like. Paragraphs, images, lists, etc.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
